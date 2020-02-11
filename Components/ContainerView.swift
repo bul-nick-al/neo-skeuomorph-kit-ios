0061b77bf94b98f4e66ac464e8ff8dad9caa05ce
Nick <bul-nick-al@ya.ru>
2020-02-11 20:17:06 +0300

    Refactor the code

--- Components/ContainerView.swift
+++ Components/ContainerView.swift
@@ -1,84 +1,70 @@
 import UIKit
 
 public class ContainerView: UIView {
-    private let darkSideColor: UIColor = UIColor(red: 0.53, green: 0.65, blue: 0.75, alpha: 0.48)
+    let darkSideColor: UIColor = UIColor(red: 0.53, green: 0.65, blue: 0.75, alpha: 0.48)
     let brightSideColor: UIColor = .white
-    lazy var outerBrightSide: CALayer = .getOuterSide(color: brightSideColor)
-    lazy var outerDarkSide: CALayer = .getOuterSide(color: darkSideColor)
-    lazy var innerBrightSide: CAShapeLayer = .getInnerSide(color: brightSideColor)
-    lazy var innerDarkSide: CAShapeLayer = .getInnerSide(color: darkSideColor.withAlphaComponent(1.0))
-    var darkSide: CALayer {
+    lazy private var outerBrightSide: CALayer = .getOuterSide(color: brightSideColor)
+    lazy private var outerDarkSide: CALayer = .getOuterSide(color: darkSideColor)
+    lazy private var innerBrightSide: CAShapeLayer = .getInnerSide(color: brightSideColor)
+    lazy private var innerDarkSide: CAShapeLayer = .getInnerSide(color: darkSideColor.withAlphaComponent(1.0))
+    var isConvex: Bool {
         get {
-            let newRadius = CGFloat(radius)
-            let size = CGFloat(shadowSize)
-            if isConvex {
-                outerDarkSide.shadowPath = UIBezierPath(roundedRect: layer.bounds, cornerRadius: newRadius).cgPath
-                outerDarkSide.shadowOffset = CGSize(width: size / 2, height: size / 4)
-                outerDarkSide.shadowRadius = size
-                return outerDarkSide
-            } else {
-                innerDarkSide.path = makeInnerUpperShadowShapePath(rect: bounds, radius: newRadius)
-                innerDarkSide.shadowOffset = CGSize(width: -size / 2, height: -size / 4)
-                innerDarkSide.shadowRadius = -size
-                return innerDarkSide
-            }
+            return elevation > 0
         }
     }
-    var brightSide: CALayer {
-        get {
-            let newRadius = CGFloat(radius)
-            let size = CGFloat(shadowSize)
-            if isConvex {
-                outerBrightSide.shadowPath = UIBezierPath(roundedRect: layer.bounds, cornerRadius: newRadius).cgPath
-                outerBrightSide.shadowOffset = CGSize(width: -size / 2, height: -size / 4)
-                outerBrightSide.shadowRadius = size
-                return outerBrightSide
-            } else {
-                innerBrightSide.path = makeInnerLowerShadowShapePath(rect: bounds, radius: newRadius)
-                innerBrightSide.shadowOffset = CGSize(width: size / 2, height: size / 4)
-                innerBrightSide.shadowRadius = -size
-                return innerBrightSide
-            }
+    public var elevation: Float = 0 {
+        didSet {
+            setNeedsLayout()
         }
     }
-    func makeInnerUpperShadowShapePath(rect: CGRect, radius: CGFloat) -> CGPath {
-        let path = CGPath.makeInnerShadowShapePathFor(rect: rect, withCornerRadius: radius)
-        var transform = CGAffineTransform(translationX: -1, y: -1)
-        return path.copy(using: &transform)!
+    public var cornerRadius: Float = 0 {
+        didSet {
+            setNeedsLayout()
+        }
     }
-    func makeInnerLowerShadowShapePath(rect: CGRect, radius: CGFloat) -> CGPath {
-        let path = CGPath.makeInnerShadowShapePathFor(rect: rect, withCornerRadius: radius)
-        var transform = CGAffineTransform(translationX: rect.maxX + 1, y: rect.maxY + 1)
-        transform = transform.rotated(by: .pi)
-        return path.copy(using: &transform)!
+    public override var backgroundColor: UIColor? {
+        didSet {
+            surface.backgroundColor = backgroundColor?.cgColor
+        }
     }
     var surface: CALayer = {
         let surface = CALayer()
         surface.backgroundColor =  UIColor(red: 227.0/255.0, green: 237.0/255.0, blue: 247.0/255.0, alpha: 1.0).cgColor
         return surface
     }()
-    var isConvex: Bool {
+    private var darkSide: CALayer {
         get {
-            return shadowSize > 0
+            if isConvex {
+                outerDarkSide.shadowPath = UIBezierPath(roundedRect: layer.bounds, cornerRadius: cornerRadius.cgFloat).cgPath
+                outerDarkSide.shadowOffset = CGSize(width: elevation.cgFloat / 2, height: elevation.cgFloat / 4)
+                outerDarkSide.shadowRadius = elevation.cgFloat
+                return outerDarkSide
+            } else {
+                innerDarkSide.path = CGPath.makeInnerUpperShadowShapePath(rect: bounds, radius: cornerRadius.cgFloat)
+                innerDarkSide.shadowOffset = CGSize(width: -elevation.cgFloat / 2, height: -elevation.cgFloat / 4)
+                innerDarkSide.shadowRadius = -elevation.cgFloat
+                return innerDarkSide
+            }
         }
     }
-    public var shadowSize: Float = 0 {
-        didSet {
-            setNeedsLayout()
-        }
-    }
-    override public var bounds: CGRect {
-        didSet {
-            surface.frame = layer.bounds
-        }
-    }
-    public var radius: Float = 0 {
-        didSet {
-            surface.cornerRadius = CGFloat(radius)
-            setNeedsLayout()
+    private var brightSide: CALayer {
+        get {
+            if isConvex {
+                outerBrightSide.shadowPath = UIBezierPath(roundedRect: layer.bounds, cornerRadius: cornerRadius.cgFloat).cgPath
+                outerBrightSide.shadowOffset = CGSize(width: -elevation.cgFloat / 2, height: -elevation.cgFloat / 4)
+                outerBrightSide.shadowRadius = elevation.cgFloat
+                return outerBrightSide
+            } else {
+                innerBrightSide.path = CGPath.makeInnerLowerShadowShapePath(rect: bounds, radius: cornerRadius.cgFloat)
+                innerBrightSide.shadowOffset = CGSize(width: elevation.cgFloat / 2, height: elevation.cgFloat / 4)
+                innerBrightSide.shadowRadius = -elevation.cgFloat
+                return innerBrightSide
+            }
         }
     }
     private func updateView() {
+        surface.frame = bounds
+        surface.cornerRadius = CGFloat(cornerRadius)
         layer.sublayers = []
         if isConvex {
             layer.mask = nil
@@ -87,7 +73,7 @@ public class ContainerView: UIView {
             layer.addSublayer(surface)
         } else {
             let maskLayer = CAShapeLayer()
-            maskLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: CGFloat(radius)).cgPath
+            maskLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: CGFloat(cornerRadius)).cgPath
             layer.mask = maskLayer
             layer.addSublayer(surface)
             layer.addSublayer(darkSide)
@@ -97,12 +83,16 @@ public class ContainerView: UIView {
 
     override public func layoutSubviews() {
         super.layoutSubviews()
-        bounds = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
         updateView()
     }
 }
 
 extension CALayer {
+    /**
+     Get a layer to be used as the outer side of a container view
+     
+     - parameter color: the color of the shadow this layer will have.
+     */
     static func getOuterSide(color: UIColor) -> CALayer {
         let outerSide = CALayer()
         outerSide.shadowColor = color.cgColor
@@ -110,6 +100,12 @@ extension CALayer {
         outerSide.masksToBounds = false
         return outerSide
     }
+    
+    /**
+    Get a layer to be used as the inner side of a container view
+    
+    - parameter color: the color of the shadow this layer will have.
+    */
     static func getInnerSide(color: UIColor) -> CAShapeLayer {
         let innerSide = CAShapeLayer()
         innerSide.fillRule = .evenOdd
@@ -122,6 +118,23 @@ extension CALayer {
 }
 
 extension CGPath {
+    /**
+     This method creates a thick path in the shape of two adjacent sides of a round rectangle
+     - parameter rect: the rectangle.
+     - parameter radius: the radius of the rounded corners.
+     - returns: the path.
+     
+     # Shape #
+     ```
+        _______
+      //       \\
+     //
+     ||
+     ||
+     ||
+     \\
+     ```
+    */
     static func makeInnerShadowShapePathFor(rect: CGRect, withCornerRadius radius: CGFloat) -> CGPath {
         let path = CGMutablePath()
         let height = rect.height + 1
@@ -139,5 +152,21 @@ extension CGPath {
         path.addArc(center: CGPoint(x: radius, y: height - radius), radius: radius + 10, startAngle: .pi,
                     endAngle: 3/4 * .pi, clockwise: true)
         return path
+    }
+    static func makeInnerUpperShadowShapePath(rect: CGRect, radius: CGFloat) -> CGPath {
+        let path = CGPath.makeInnerShadowShapePathFor(rect: rect, withCornerRadius: radius)
+        var transform = CGAffineTransform(translationX: -1, y: -1)
+        return path.copy(using: &transform)!
+    }
+    static func makeInnerLowerShadowShapePath(rect: CGRect, radius: CGFloat) -> CGPath {
+        let path = CGPath.makeInnerShadowShapePathFor(rect: rect, withCornerRadius: radius)
+        var transform = CGAffineTransform(translationX: rect.maxX + 1, y: rect.maxY + 1)
+        transform = transform.rotated(by: .pi)
+        return path.copy(using: &transform)!
+    }
+}
+extension Float {
+    var cgFloat: CGFloat {
+        return CGFloat(self)
     }
 }
